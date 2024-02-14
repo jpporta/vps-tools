9ffc0d93b3edf39392bda2cce725e6d6621f54d6
root <root@joao.porta>
2024-02-14 16:53:11 -0300

    docker

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { invoicePlugin } from "./invoice";
 const app = new Elysia()
   .use(invoicePlugin)
   .get("/", () => "Hello Elysia")
-  .listen(3000);
+  .listen(Bun.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
