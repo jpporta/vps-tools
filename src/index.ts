ea494133d776dfe3c6cb13417daacca0b8f05241
Joao Porta <jpedro.porta@gmail.com>
2025-09-23 15:14:10 -0300

    update invoice format to follow new pattern

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { Elysia } from "elysia";
+import { Elysia} from "elysia";
 import { invoicePlugin } from "./invoice";
+import { staticPlugin } from "@elysiajs/static";
 
 const app = new Elysia()
   .use(invoicePlugin)
+  .use(staticPlugin())
   .get("/", () => "Hello Elysia")
   .listen(Bun.env.PORT || 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
--- src/invoice.tsx
+++ src/invoice.tsx
@@ -1,5 +1,5 @@
 import { Elysia } from "elysia";
-import { html } from "@elysiajs/html";
+import { html, Html } from "@elysiajs/html";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 
@@ -18,259 +18,191 @@ const Invoice = ({
   dueBalance: string;
   hours: number;
   rate: number;
-  detailed: { day: string; total: string }[];
+  detailed: {
+    day: string;
+    total: string;
+    hours: number;
+  }[];
 }) => (
   <html>
     <head>
       <title>Invoice</title>
+      <link rel="stylesheet" href="/public/style.css" />
     </head>
-    <style>
-      {`
-    @page {
-	size: legal;
-
-    }
-* {
-margin: 0;
-padding: 0;
-box-sizing: border-box;
- }
-body {
-font-family: Arial, Helvetica, sans-serif;
--webkit-print-color-adjust:exact !important;
-  print-color-adjust:exact !important;
-width: 8.5in;
-height: 14in;
-padding: 0.25in;
-}
-.bold {
-	font-weight: bold;
-}
-.title, .summary {
-display: flex;
-justify-content: space-between;
-margin-bottom: 4rem;
-}
-
-hgroup {
-text-align: right;
-}
-h1 {
-font-size: 3rem;
-}
-hgroup > h2 {
-font-size: 1rem;
-color: #666;
-margin-top: 0.5rem;
-}
-.myInfo {
-margin-top: 0.5rem;
-}
-.billTo > span{
-color: #666;
-}
-.balance{
-display: flex;
-flex-direction: column;
-gap: 0.5rem;
-min-width: 30%;
-}
-.balance > div {
-display: flex;
-justify-content: end;
-gap: 2rem;
-padding: 0.5rem 1rem;
-border-radius: 0.25rem;
-}
-.balance p {
-text-align: right;
-}
-.gray-bg {
-background-color: #eee;
-}
-
-th, td {
-padding: 0.5rem;
-}
-
-tbody.colored > tr:nth-child(even) {
-background-color: #f2f2f2;
-}
-
-
-`}
-    </style>
-
     <body>
       <header>
         <div class="title">
-          <div class="myInfo">
-            <p class="bold">JOAO PEDRO PIN PORTA 40174460864</p>
-            <p>R LUIS ROBERTO GOES, 176, G-17</p>
-            <p>PARQUE BRASIL 500 - PAULINIA - SP</p>
-            <p>Zip Code: 13.141-064</p>
-          </div>
-          <hgroup>
-            <h1>INVOICE</h1>
-            <h2>#{invoiceNumber}</h2>
-          </hgroup>
-        </div>
+          <div>
+            <div class="myInfo">
+              <span>FROM:</span>
+              <p class="bold" style="margin-top: 0.5rem">
+                JOAO PEDRO PIN PORTA 40174460864
+              </p>
+              <p>jpedro.porta@gmail.com</p>
+              <p>R LUIS ROBERTO GOES, 176, G-17</p>
+              <p>PARQUE BRASIL 500 - PAULINIA - SP</p>
+              <p>Zip Code: 13.141-064</p>
+            </div>
 
-        <div class="summary">
-          <div class="billTo">
-            <span>Bill To:</span>
-            <p class="bold" style="margin-top: 0.5rem;">
-              Avodah, Inc. (Corporate)
-            </p>
-            <p>Office Address: 1600 SOLANA BLVD, SUITE 8110,</p>
-            <p>WESTLAKE, TX 76262</p>
-            <p>Phone: 817.588.7100</p>
+            <div class="billTo">
+              <span>TO:</span>
+              <p class="bold" style="margin-top: 0.5rem;">
+                Avodah, Inc. (Corporate)
+              </p>
+              <p>Office Address: 1600 SOLANA BLVD, SUITE 8110,</p>
+              <p>WESTLAKE, TX 76262</p>
+              <p>Phone: 817.588.7100</p>
+            </div>
           </div>
-          <div class="balance">
-            <div class="row">
-              <p>Date:</p>
-              <p>{currentDate}</p>
-            </div>
-            <div class="row">
-              <p>Due Date:</p>
-              <p>{dueDate}</p>
-            </div>
-            <div class="row gray-bg">
-              <p class="bold">Balance Due:</p>
-              <p class="bold">{dueBalance}</p>
+          <div>
+            <hgroup>
+              <h1>INVOICE</h1>
+              <h2>#{invoiceNumber}</h2>
+            </hgroup>
+            <div class="balance">
+              <div class="row">
+                <p>Date:</p>
+                <p>{currentDate}</p>
+              </div>
+              <div class="row">
+                <p>Due Date:</p>
+                <p>{dueDate}</p>
+              </div>
+              <div class="row gray-bg">
+                <p class="bold">Balance Due:</p>
+                <p class="bold">{dueBalance}</p>
+              </div>
             </div>
           </div>
         </div>
+        <h4>
+          Departament: <span style="color: #F27054">AvodahMed</span> Engineering
+        </h4>
       </header>
       <main>
         <table style="width: 100%">
-          <thead style="background-color: #444; color: white">
+          <thead style="background-color: black; color: white">
             <tr>
-              <td style="padding: 0.5rem 1rem;border: none;">Item</td>
-              <td style="padding: 0.5rem 1rem;border: none;">Quantity</td>
+              <td style="padding: 0.5rem 1rem;border: none;">Date</td>
+              <td style="padding: 0.5rem 1rem;border: none;">
+                Task Description
+              </td>
+              <td style="padding: 0.5rem 1rem;border: none;">Hours Worked</td>
               <td style="padding: 0.5rem 1rem;border: none;">Rate</td>
-              <td style="padding: 0.5rem 1rem;border: none;">Amount</td>
+              <td style="padding: 0.5rem 1rem;border: none;">Total</td>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <p class="bold">Summary:</p>
-                <p style="color: #666">
-                  {hours} hours * US${rate}/hour = {dueBalance}
-                </p>
-              </td>
-              <td>1</td>
-              <td>{dueBalance}</td>
-              <td>{dueBalance}</td>
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td>
-                <p>Subtotal:</p>
-              </td>
-              <td>
-                <p style="min-width: 8rem;">{dueBalance}</p>
-              </td>
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td>
-                <p>Tax:</p>
-              </td>
-              <td>
-                <p style="min-width: 8rem;">US$ 0,00</p>
-              </td>
-            </tr>
-            <tr>
-              <td />
-              <td />
-              <td>
-                <p>Total:</p>
-              </td>
-              <td>
-                <p style="min-width: 8rem;">{dueBalance}</p>
-              </td>
-            </tr>
-            <tr style="height:0.5rem" />
-            <tr>
-              <td colspan="2" style="vertical-align: bottom">
-                <p style="color: #666;">Notes:</p>
-              </td>
-              <td style="background-color: #444; color: white">
-                <span>Date</span>
-              </td>
-              <td style="background-color: #444; color: white">
-                <span>Worked Hours</span>
-              </td>
-            </tr>
-            <tr>
-              <td colspan="2" rowspan={detailed.length + 1}>
-                <div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Benificiary</p>
-                    <p>JOAO PEDRO PIN PORTA 40174460864</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Address</p>
-                    <p>Campinas, Brazil</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>IBAN</p>
-                    <p>JOAO PEDRO PIN PORTA 40174460864</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Benificiary</p>
-                    <p>BR1178632767000010005264481C1</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>SWIFT Code</p>
-                    <p>OURIBRSPXXX</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Branch Code</p>
-                    <p>00001</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Bank Name</p>
-                    <p>BANCO OURINVEST S.A</p>
-                  </div>
-                  <div style="margin-bottom: 1rem;">
-                    <p>Bank Address</p>
-                    <p>Sao Paulo, Brazil</p>
-                  </div>
-                </div>
-              </td>
-            </tr>
-
-            {detailed.map((day, idx) => (
-              <tr
-                style={`vertical-align: top; ${idx !== detailed.length - 1 ? "height: 2rem; " : ""}${
-                  (idx % 2) - (detailed.length % 2) === 0
-                    ? "background-color: #f2f2f2"
-                    : ""
-                }`}
-              >
-                <td>{day.day}</td>
-                <td>{day.total}</td>
+            {detailed.map(({ day, total, hours }) => (
+              <tr>
+                <td>{day}</td>
+                <td style="font-style: italic">Worked on AvodahMed</td>
+                <td>{total}</td>
+                <td>US$ {rate} / hr</td>
+                <td>US$ {Number(hours * rate).toFixed(2)}</td>
               </tr>
             ))}
+            <tr>
+              <td colspan="2" style="text-align: right">
+                Hours Subtotal
+              </td>
+              <td>{hours}h</td>
+              <td>US$ {rate} / hr</td>
+              <td>US$ {Number(hours * rate).toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td colspan="4" style="text-align: right">
+                Subtotal:
+              </td>
+              <td>US$ {dueBalance}</td>
+            </tr>
+            <tr>
+              <td colspan="4" style="text-align: right">
+                Tax:
+              </td>
+              <td>US$ 0,00</td>
+            </tr>
+            <tr>
+              <td colspan="4" style="text-align: right">
+                Total:
+              </td>
+              <td>{dueBalance}</td>
+            </tr>
           </tbody>
         </table>
+
+        <footer>
+          <div>
+            <h3>Payment Information</h3>
+            <div style="margin-bottom: 1rem;">
+              <p>Benificiary</p>
+              <p>JOAO PEDRO PIN PORTA 40174460864</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>Address</p>
+              <p>Campinas, Brazil</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>IBAN</p>
+              <p>JOAO PEDRO PIN PORTA 40174460864</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>Benificiary</p>
+              <p>BR1178632767000010005264481C1</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>SWIFT Code</p>
+              <p>OURIBRSPXXX</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>Branch Code</p>
+              <p>00001</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>Bank Name</p>
+              <p>BANCO OURINVEST S.A</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <p>Bank Address</p>
+              <p>Sao Paulo, Brazil</p>
+            </div>
+          </div>
+          <div>
+            <h3>
+              Payment Instructions
+              <br />
+              Terms and Conditions:
+            </h3>
+            <p style="max-width: 50vw; font-size: 0.9rem; line-height: 1.4rem">
+              Payment due within contract terms (within 15 days)
+              <br />
+              Invoice Due Date: Semi-Monthly (15th and Last Day of the Month)
+              <br />
+              Invoice Submission Detail: Please forward a copy of this invoice
+              to your direct supervisor or manager for approval. Additionally,
+              include accounting@avodah.com for visibility.
+              <br />
+              Once your supervisor or manager has approved the invoice, the
+              Avodah Accounting team will process it for payment.
+            </p>
+          </div>
+        </footer>
       </main>
     </body>
   </html>
 );
 export const invoicePlugin = new Elysia({ prefix: "/invoice" })
   .use(html())
-  .get("/", async ({ query: { days, rate = 62.5, offset = 0 } }) => {
+  .get("/", async ({ query: { days, rate = 62.5, offset = 0 }, set }) => {
+    set.headers["Content-Type"] = "text/html";
     let hours;
     dayjs.extend(duration);
     const currentDate = dayjs(new Date()).format("MMM DD, YYYY");
     const dueDate = dayjs(new Date()).add(15, "days").format("MMM DD, YYYY");
-    let detailed: { Date: string; Total: number }[] = [];
+    let detailed: {
+      Date: string;
+      Total: number;
+    }[] = [];
     if (days) {
       hours = Number(days) * 8;
     } else {
@@ -281,7 +213,10 @@ export const invoicePlugin = new Elysia({ prefix: "/invoice" })
         Total: number;
         Start: string;
         End: string;
-        Detailed: { Date: string; Total: number }[];
+        Detailed: {
+          Date: string;
+          Total: number;
+        }[];
       };
       hours = Total;
       detailed = Detailed;
@@ -301,6 +236,7 @@ export const invoicePlugin = new Elysia({ prefix: "/invoice" })
       detailed: detailed.map((day) => ({
         day: dayjs(day.Date.split("T")[0]).format("MMM DD, YYYY"),
         total: dayjs.duration(day.Total, "hours").format("HH[h]mm[m]"),
+        hours: day.Total,
       })),
     };
 
